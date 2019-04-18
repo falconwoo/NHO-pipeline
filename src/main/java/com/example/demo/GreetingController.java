af28e232f4e0fd927f0ab48a43e317d22b9b1d22
Wu Xueheng <xhwu@thoughtworks.com>
2019-04-18 16:49:17 +0800

    First commit

---
+++ src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value = "/api/greeting")
+public class GreetingController {
+
+    private static final String GREETING = "How are you, ";
+
+    @GetMapping()
+    public String greeting(@RequestParam("name") String name) {
+        return GREETING + name;
+    }
+}
